ce1b2dd5a3472c3b3a7bf19f1b309496de3ae74e
Abhay-Shankur <abhayshankur3812.com>
2024-04-22 12:00:00 +0530

    Web Demo Template 2

--- src/index.js
+++ src/index.js
@@ -3,30 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import firebase from 'firebase/app'; // Import Firebase core module
-import 'firebase/analytics'; // Import Firebase Analytics module
-
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC2NOiY3zn0Xh7RSG1RGkTx7Gl8XMFsTgE",
-  authDomain: "call-infos.firebaseapp.com",
-  projectId: "call-infos",
-  storageBucket: "call-infos.appspot.com",
-  messagingSenderId: "840804333118",
-  appId: "1:840804333118:web:300562b67e266e6dd4c36f",
-  measurementId: "G-T2Y4XMBT9Y"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC2NOiY3zn0Xh7RSG1RGkTx7Gl8XMFsTgE",
+//   authDomain: "call-infos.firebaseapp.com",
+//   projectId: "call-infos",
+//   storageBucket: "call-infos.appspot.com",
+//   messagingSenderId: "840804333118",
+//   appId: "1:840804333118:web:300562b67e266e6dd4c36f",
+//   measurementId: "G-T2Y4XMBT9Y"
+// };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
